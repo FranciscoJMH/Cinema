c70260096f2deed85a4b99c72aa43f7675eafb17
Francisco Michel <jav_michel@hotmail.com>
2020-03-27 18:14:39 -0600

    Conectar pagina de inicio con peliculas

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 
 gem 'bootstrap', '~> 4.4.1'
+gem "bootstrap_form", "~> 4.0"
+
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   def home
+    @movies = Movie.all.order(publish: :asc)
+    @categories = Movie.all&.pluck(:category)&.uniq
     render 'layouts/home'
   end
 end
